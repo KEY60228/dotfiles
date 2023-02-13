6d008f131109f5a6128caac931585356a98cecd1
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2023-02-13 23:20:36 +0900

    add gcc to .aliases.zsh

--- .aliases.zsh
+++ .aliases.zsh
@@ -4,3 +4,7 @@ alias dcu='docker-compose up -d'
 alias dcd='docker-compose down'
 alias dcr='docker-compose restart'
 alias dcl='docker-compose logs -f'
+
+########## gcc ##########
+alias gcc='gcc-12'
+alias g++='g++-12'

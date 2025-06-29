7a6e1a52da369e6b6b32c2ea421d79357f0468f8
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2025-06-30 00:46:35 +0900

    setup toggleterm

---
+++ nvim/lua/plugins/toggleterm.lua
@@ -0,0 +1,17 @@
+return {
+  'akinsho/toggleterm.nvim',
+  version = "*",
+  config = function()
+    require("toggleterm").setup({
+      -- size can be a number or function which is passed the current terminal
+      size = 20,
+      open_mapping = [[<M-j>]],
+      hide_numbers = true,
+      direction = 'float',
+      float_opts = {
+        border = 'single',
+      },
+    })
+  end
+}
+

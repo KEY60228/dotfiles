a52190690e97262a8dcd0d8188ea87541d72d432
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2025-06-29 18:20:38 +0900

    setup nvim-treesitter

---
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -0,0 +1,52 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  branch = 'master',
+  lazy = false,
+  build = ":TSUpdate",
+  config = function()
+    require("nvim-treesitter.configs").setup({
+      ensure_installed = {
+        "bash",
+        "c",
+        "cmake",
+        "cpp",
+        "css",
+        "csv",
+        "dockerfile",
+        "git_config",
+        "git_rebase",
+        "gitattributes",
+        "gitcommit",
+        "gitignore",
+        "go",
+        "helm",
+        "html",
+        "javascript",
+        "jq",
+        "jsdoc",
+        "lua",
+        "make",
+        "markdown",
+        "query",
+        "sql",
+        "terraform",
+        "toml",
+        "tsx",
+        "typescript",
+        "vim",
+        "vimdoc",
+        "xml",
+        "yaml",
+      },
+      highlight = {
+        enable = true,
+      },
+      indent = {
+        enable = true,
+      },
+      sync_install = false,
+      auto_install = true,
+    })
+  end,
+}
+

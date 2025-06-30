520cc4f47bf52c8e036c6f336a2a30f85250b4a2
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2025-07-01 06:33:44 +0900

    setup nvim-cmp & luasnip

---
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -0,0 +1,62 @@
+return {
+  "hrsh7th/nvim-cmp",
+  dependencies = {
+    "hrsh7th/cmp-nvim-lsp",
+  },
+  config = function()
+    local cmp = require("cmp")
+    local luasnip = require("luasnip")
+
+    cmp.setup({
+      sources = {
+        { name = "nvim_lsp" },
+        { name = "luasnip" },
+      },
+      snippet = {
+        expand = function(args)
+          luasnip.lsp_expand(args.body)
+        end
+      },
+      mapping = cmp.mapping.preset.insert({
+        ["<C-e>"] = cmp.mapping.close(),
+        ["<M-k>"] = cmp.mapping.complete(),
+        ['<CR>'] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            if luasnip.expandable() then
+              luasnip.expand()
+            else
+              cmp.confirm({
+                select = true,
+              })
+            end
+          else
+            fallback()
+          end
+        end),
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+          elseif luasnip.locally_jumpable(1) then
+            luasnip.jump(1)
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif luasnip.locally_jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+      })
+    })
+
+    vim.lsp.config("*", {
+      capabilities = require("cmp_nvim_lsp").default_capabilities()
+    })
+  end
+}
+

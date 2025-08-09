16b49d95d62e09f0717b1418dcae40ab75faec7f
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2025-08-09 15:23:02 +0900

    add auto switcher to bufferline

--- nvim/lua/plugins/bufferline.lua
+++ nvim/lua/plugins/bufferline.lua
@@ -2,11 +2,27 @@
 vim.keymap.set({ "n", "i" }, "<C-Tab>", "<cmd>bnext<CR>")
 vim.keymap.set({ "n", "i" }, "<C-S-Tab>", "<cmd>bprev<CR>")
 
+-- Auto switch to next buffer when closing
+vim.api.nvim_create_autocmd("BufDelete", {
+  callback = function()
+    vim.schedule(function()
+      local nvim_tree = require("nvim-tree.api")
+      local buffers = vim.fn.getbufinfo({ buflisted = 1 })
+
+      -- If there are other buffers and nvim-tree is focused, switch to another buffer
+      if #buffers > 0 and nvim_tree.tree.is_tree_buf() then
+        vim.cmd("bnext")
+      end
+    end)
+  end,
+})
+
 return {
   "akinsho/bufferline.nvim",
   version = "*",
   dependencies = {
-    "nvim-tree/nvim-web-devicons"
+    "nvim-tree/nvim-tree.lua",
+    "nvim-tree/nvim-web-devicons",
   },
   config = function ()
     require("bufferline").setup({})

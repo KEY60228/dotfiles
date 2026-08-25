161fe4c888665a2fe20ae4f8fdbb01b559b423e9
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2026-08-25 18:47:00 +0900

    fix reviewthem.lua

--- dot_config/nvim/lua/plugins/reviewthem.lua
+++ dot_config/nvim/lua/plugins/reviewthem.lua
@@ -1,14 +1,7 @@
 return {
   "KEY60228/reviewthem.nvim",
-  dependencies = {
-    "sindrets/diffview.nvim",
-    "nvim-telescope/telescope.nvim",
-  },
   config = function()
-    require("reviewthem").setup({
-      diff_tool = "alt-diffview",
-      ui = "telescope",
-    })
+    require("reviewthem").setup()
   end,
 }
 
